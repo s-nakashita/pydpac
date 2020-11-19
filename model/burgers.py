62e947f6577d2d7414526f2906e0a289eafc07ba
nakashita <>
2020-11-19 07:21:14 +0000

    Class added to model
    FossilOrigin-Name: 37189e15d365fa0e21a5a8317698b90feeccfa4c42346a618fa4d7d1830407e4

--- model/burgers.py
+++ model/burgers.py
@@ -1,22 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def l_operator(f, u, dx, nu):
+class Bg():
+    def __init__(self, dx, dt, nu):
+        self.dx = dx
+        self.dt = dt
+        self.nu = nu
+
+    def l_operator(self, f, u):
 # NB u is defined at integral index
 # f[i-1/2] = (f[i-1] + f[i]) / 2
 # f[i+1/2] = (f[i] + f[i+1]) / 2
 # f[i+1/2] - f[i-1/2] = (f[i+1] - f[i-1]) / 2
-    l = np.zeros_like(u)
-    l[1:-1] = -0.5 * (f[2:] - f[0:-2]) / dx \
-        + nu / dx**2 * (u[2:] - 2 * u[1:-1] + u[0:-2])
-    return l
+        l = np.zeros_like(u)
+        l[1:-1] = -0.5 * (f[2:] - f[0:-2]) / self.dx \
+            + self.nu / self.dx**2 * (u[2:] - 2 * u[1:-1] + u[0:-2])
+        return l
 
 
-def step(u, dx, dt, nu):
-    f = 0.5 * u**2
-    #print("f={}".format(f))
-    u1 = u + dt * l_operator(f, u, dx, nu)
-    return 0.5 * (u + u1 + dt * l_operator(f, u1, dx, nu))
+    def step(self, u):
+        f = 0.5 * u**2
+        #print("f={}".format(f))
+        u1 = u + self.dt * self.l_operator(f, u)
+        return 0.5 * (u + u1 + self.dt * self.l_operator(f, u1))
 
 
 if __name__ == "__main__":
@@ -33,6 +39,8 @@ if __name__ == "__main__":
     umax = np.amax(np.abs(u))
     c = umax * dt / dx
     nt = int(tmax / dt) + 1
+    
+    burgers = Bg(dx, dt, nu)
 
     print("n={} nu={} dt={:7.3e} tmax={} tsave={}".format(n, nu, dt, tmax, tsave))
     print("R={} dx={:7.3e} umax={} c={} nt={}".format(reynolds_number, dx, umax, c, nt))
@@ -45,5 +53,5 @@ if __name__ == "__main__":
         if k * dt % tsave == 0:
                 np.savetxt("u{:05d}.txt".format(k), u)
                 ax.plot(x, u)
-        u = step(u, dx, dt, nu)
+        u = burgers.step(u)
     fig.savefig("u.png")
--- model/lorenz.py
+++ model/lorenz.py
@@ -1,82 +1,89 @@
 import numpy as np
 
-def l96(x, F):
-    l = np.zeros_like(x)
-    l = (np.roll(x, -1, axis=0) - np.roll(x, 2, axis=0)) * np.roll(x, 1, axis=0) - x + F
-    return l
+class L96():
+    def __init__(self, dt, F):
+        self.dt = dt
+        self.F = F
 
-def step(xa, h, F):
-    k1 = h * l96(xa, F)
-    
-    k2 = h * l96(xa+k1/2, F)
-    
-    k3 = h * l96(xa+k2/2, F)
-    
-    k4 = h * l96(xa+k3, F)
-    
-    return xa + (0.5*k1 + k2 + k3 + 0.5*k4)/3.0
+    def l96(self, x):
+        l = np.zeros_like(x)
+        l = (np.roll(x, -1, axis=0) - np.roll(x, 2, axis=0)) * np.roll(x, 1, axis=0) - x + self.F
+        return l
 
-def l96_t(x, dx):
-    l = np.zeros_like(x)
-    l = (np.roll(x, -1, axis=0) - np.roll(x, 2, axis=0)) * np.roll(dx, 1, axis=0) + \
-        (np.roll(dx, -1, axis=0) - np.roll(dx, 2, axis=0)) * np.roll(x, 1, axis=0) - dx
-    return l
+    def step(self, xa):
+        k1 = self.dt * self.l96(xa)
+    
+        k2 = self.dt * self.l96(xa+k1/2)
+    
+        k3 = self.dt * self.l96(xa+k2/2)
+    
+        k4 = self.dt * self.l96(xa+k3)
+    
+        return xa + (0.5*k1 + k2 + k3 + 0.5*k4)/3.0
 
-def step_t(x, dx, h, F):
-    k1 = h * l96(x, F)
-    dk1 = h * l96_t(x, dx)
-    
-    k2 = h * l96(x+k1/2, F)
-    dk2 = h * l96_t(x+k1/2, dx+dk1/2)
-    
-    k3 = h * l96(x+k2/2, F)
-    dk3 = h * l96_t(x+k2/2, dx+dk2/2)
-    
-    k4 = h * l96(x+k3, F)
-    dk4 = h * l96_t(x+k3, dx+dk3)
-    
-    return dx + (0.5*dk1 + dk2 + dk3 + 0.5*dk4)/3.0
+    def l96_t(self, x, dx):
+        l = np.zeros_like(x)
+        l = (np.roll(x, -1, axis=0) - np.roll(x, 2, axis=0)) * np.roll(dx, 1, axis=0) + \
+            (np.roll(dx, -1, axis=0) - np.roll(dx, 2, axis=0)) * np.roll(x, 1, axis=0) - dx
+        return l
 
-def l96_adj(x, dx):
-    l = np.zeros_like(x)
-    l = np.roll(x, 2, axis=0) * np.roll(dx, 1, axis=0) + \
-        (np.roll(x, -2, axis=0) - np.roll(x, 1, axis=0)) * np.roll(dx, -1, axis=0) - \
-        np.roll(x, -1, axis=0) * np.roll(dx, -2, axis=0) - dx
-    return l
+    def step_t(self, x, dx):
+        k1 = self.dt * self.l96(x)
+        dk1 = self.dt * self.l96_t(x, dx)
+    
+        k2 = self.dt * self.l96(x+k1/2)
+        dk2 = self.dt * self.l96_t(x+k1/2, dx+dk1/2)
+    
+        k3 = self.dt * self.l96(x+k2/2)
+        dk3 = self.dt * self.l96_t(x+k2/2, dx+dk2/2)
+    
+        k4 = self.dt * self.l96(x+k3)
+        dk4 = self.dt * self.l96_t(x+k3, dx+dk3)
+    
+        return dx + (0.5*dk1 + dk2 + dk3 + 0.5*dk4)/3.0
+
+    def l96_adj(self, x, dx):
+        l = np.zeros_like(x)
+        l = np.roll(x, 2, axis=0) * np.roll(dx, 1, axis=0) + \
+            (np.roll(x, -2, axis=0) - np.roll(x, 1, axis=0)) * np.roll(dx, -1, axis=0) - \
+            np.roll(x, -1, axis=0) * np.roll(dx, -2, axis=0) - dx
+        return l
 # np.roll(X0,2,axis=0)*np.roll(dXb,1,axis=0) + (np.roll(X0,-2,axis=0) - np.roll(X0,1,axis=0))*np.roll(dXb,-1,axis=0) - 
 # np.roll(X0,-1,axis=0)*np.roll(dXb,-2,axis=0) - dXb
-def step_adj(x, dx, h, F):
-    k1 = h * l96(x, F)
-    x2 = x + 0.5*k1
-    k2 = h * l96(x2, F)
-    x3 = x + 0.5*k2
-    k3 = h * l96(x3, F)
-    x4 = x + k3
-    k4 = h * l96(x4, F)
+    def step_adj(self, x, dx):
+        k1 = self.dt * self.l96(x)
+        x2 = x + 0.5*k1
+        k2 = self.dt * self.l96(x2)
+        x3 = x + 0.5*k2
+        k3 = self.dt * self.l96(x3)
+        x4 = x + k3
+        k4 = self.dt * self.l96(x4)
 
-    dxa = dx
-    dk1 = dx / 6
-    dk2 = dx / 3
-    dk3 = dx / 3
-    dk4 = dx / 6
+        dxa = dx
+        dk1 = dx / 6
+        dk2 = dx / 3
+        dk3 = dx / 3
+        dk4 = dx / 6
 
-    dxa = dxa + h * l96_adj(x4, dk4)
-    dk3 = dk3 + h * l96_adj(x4, dk4)
+        dxa = dxa + self.dt * self.l96_adj(x4, dk4)
+        dk3 = dk3 + self.dt * self.l96_adj(x4, dk4)
 
-    dxa = dxa + h * l96_adj(x3, dk3)
-    dk2 = dk2 + 0.5 * h * l96_adj(x3, dk3)
+        dxa = dxa + self.dt * self.l96_adj(x3, dk3)
+        dk2 = dk2 + 0.5 * self.dt * self.l96_adj(x3, dk3)
 
-    dxa = dxa + h * l96_adj(x2, dk2)
-    dk1 = dk1 + 0.5 * h * l96_adj(x2, dk2)
+        dxa = dxa + self.dt * self.l96_adj(x2, dk2)
+        dk1 = dk1 + 0.5 * self.dt * self.l96_adj(x2, dk2)
 
-    dxa = dxa + h * l96_adj(x, dk1)
+        dxa = dxa + self.dt * self.l96_adj(x, dk1)
 
-    return dxa
+        return dxa
 
 if __name__ == "__main__":
     n = 40
     F = 8.0
     h = 0.05
+
+    l96 = L96(h, F)
 
     x0 = np.ones(n)*F
     x0[19] += 0.001*F
@@ -84,20 +91,20 @@ if __name__ == "__main__":
     nt = int(tmax/h) + 1
 
     for k in range(nt):
-        x0 = step(x0, h, F)
+        x0 = l96.step(x0)
         #x0 = x
     print(x0)
 
     a = 1e-5
     x0 = np.ones(n)
     dx = np.random.randn(x0.size)
-    adx = step(x0+a*dx, h, F)
-    ax = step(x0, h, F)
-    jax = step_t(x0, dx, h, F)
+    adx = l96.step(x0+a*dx)
+    ax = l96.step(x0)
+    jax = l96.step_t(x0, dx)
     d = np.sqrt(np.sum((adx-ax)**2)) / a / (np.sqrt(np.sum(jax**2)))
     print("TLM check diff.={}".format(d-1))
 
-    ax = step_t(x0, dx, h, F)
-    atax = step_adj(x0, ax, h, F)
+    ax = l96.step_t(x0, dx)
+    atax = l96.step_adj(x0, ax)
     d = (ax.T @ ax) - (dx.T @ atax)
     print("ADJ check diff.={}".format(d))
