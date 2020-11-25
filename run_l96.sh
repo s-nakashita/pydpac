2d171bc6cd3ec24f4864a163837f09cf1838c201
nakashita <>
2020-11-25 07:20:21 +0000

    running script moved
    FossilOrigin-Name: 7f6d13f433ee8f7556da8411e58f978896b8db2ce3c9185e4e03b4dee248e93f

--- run_l96.sh
+++ run_l96.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 #operators="linear quadratic cubic quadratic-nodiff cubic-nodiff"
 operators="linear"
-perturbations="mlef etkf po srf letkf kf var var4d"
-#perturbations="var4d"
+#perturbations="mlef grad etkf po srf letkf kf var var4d"
+perturbations="etkf"
 na=100 # Number of assimilation cycle
 linf="T" # "T"->Apply inflation "F"->Not apply
 lloc="T" # "T"->Apply localization "F"->Not apply
 ltlm="T" # "T"->Use tangent linear approximation "F"->Not use
 a_window=5
-exp="comp"
+exp="cltest"
 echo ${exp}
+rm -rf ${exp}
+mkdir ${exp}
+cd ${exp}
+cp ../logging_config.ini .
 for pt in ${perturbations}; do
   for op in ${operators}; do
     echo ${op} ${pt} ${na} ${linf} ${lloc} ${ltlm} ${a_window}
@@ -17,7 +21,7 @@ for pt in ${perturbations}; do
     wait
   done
 done
-python plote.py ${operators} l96 ${na}
+python ../plote.py ${operators} l96 ${na}
 #mv l96_e_${operators}.png l96_e_${operators}_${exp}.png
 rm l96*.txt 
 rm l96*.npy 
