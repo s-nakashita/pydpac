a866acb623d2a294050b6379862c8723ec10d1f4
nakashita <>
2024-01-16 00:00:49 +0000

    new plotting scripts added
    FossilOrigin-Name: 7f85fc4ad8aa150d7256196b19d89ff5812e09789fa0c21b7ad8dd130793e1d2

---
+++ plot/plotdk_nest.py
@@ -0,0 +1,97 @@
+import sys
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.gridspec as gridspec
+from matplotlib.cm import ScalarMappable
+from matplotlib.colors import Normalize
+from scipy.interpolate import interp1d
+plt.rcParams['font.size'] = 16
+
+op = sys.argv[1]
+model = sys.argv[2]
+na = int(sys.argv[3])
+dscl = False
+if len(sys.argv)>4:
+    dscl = (sys.argv[4]=='T')
+print(dscl)
+perts = ["mlef", "mlefw", "etkf", "po", "srf", "letkf", "kf", "var","var_nest",\
+    "mlefcw","mlefy","mlefbe","mlefbm",\
+    "4detkf", "4dpo", "4dsrf", "4dletkf", "4dvar", "4dmlef"]
+cmap = "coolwarm"
+f = "truth.npy"
+if not os.path.isfile(f):
+    print("not exist {}".format(f))
+    exit
+xt = np.load(f)[:na,]
+print(xt.shape)
+nx = xt.shape[1]
+t = np.arange(na)
+ix_t = np.loadtxt("ix_true.txt")
+ix_gm = np.loadtxt("ix_gm.txt")
+ix_lam = np.loadtxt("ix_lam.txt")
+i0 = np.argmin(np.abs(ix_gm - ix_lam[0]))
+if ix_gm[i0]<ix_lam[0]: i0+=1
+i1 = np.argmin(np.abs(ix_gm - ix_lam[-1]))
+if ix_gm[i1]>ix_lam[-1]: i1-=1
+tmp_lam2gm = interp1d(ix_lam,np.eye(ix_lam.size),axis=0)
+JH2 = tmp_lam2gm(ix_gm[i0:i1+1])
+xlim = 15.0
+for pt in perts:
+    #GM
+    ## nature and analysis
+    fig, axs = plt.subplots(nrows=1,ncols=3,figsize=[12,6],\
+        constrained_layout=True,sharey=True)
+    if dscl:
+        f = "{}_xfgmonly_{}_{}.npy".format(model, op, pt)
+    else:
+        f = "{}_xfgm_{}_{}.npy".format(model, op, pt)
+    if not os.path.isfile(f):
+        print("not exist {}".format(f))
+        continue
+    xfgm = np.load(f)
+    print(xfgm.shape)
+    nx = ix_gm.size
+    mp0 = axs[0].pcolormesh(ix_gm, t, xfgm, shading='auto', \
+    cmap=cmap, norm=Normalize(vmin=-xlim, vmax=xlim))
+    axs[0].set_xticks(ix_gm[::(nx//8)])
+    axs[0].set_yticks(t[::max(1,na//8)])
+    axs[0].set_xlabel("site")
+    axs[0].set_title("GM forecast")
+    p0 = fig.colorbar(mp0,ax=axs[0],orientation="horizontal")
+    axs[0].vlines([ix_lam[0],ix_lam[-1]],0,1,\
+            colors='black',linestyle='dashdot',transform=axs[0].get_xaxis_transform())
+    #LAM
+    if dscl:
+        f = "{}_xfdscl_{}_{}.npy".format(model, op, pt)
+    else:
+        f = "{}_xflam_{}_{}.npy".format(model, op, pt)
+    if not os.path.isfile(f):
+        print("not exist {}".format(f))
+        continue
+    xflam = np.load(f)
+    print(xflam.shape)
+    nx = ix_lam.size
+    mp1 = axs[1].pcolormesh(ix_lam, t, xflam, shading='auto', \
+    cmap=cmap, norm=Normalize(vmin=-xlim, vmax=xlim))
+    p1 = fig.colorbar(mp1,ax=axs[1],orientation="horizontal")
+    axs[1].set_xticks(ix_lam[::(nx//8)])
+    axs[1].set_yticks(t[::max(1,na//8)])
+    axs[1].set_xlabel("site")
+    axs[1].set_title("LAM forecast")
+    axs[1].set_xlim(ix_lam[0],ix_lam[-1])
+    #dk
+    xd = xfgm[:,i0:i1+1] - (JH2 @ xflam[:,:].T).T
+    vlim = np.nanmax(np.abs(xd))
+    mp2 = axs[2].pcolormesh(ix_gm[i0:i1+1], t, xd, shading='auto', \
+    cmap=cmap, norm=Normalize(vmin=-vlim, vmax=vlim))
+    axs[2].set_xticks(ix_gm[i0:i1+1:8])
+    axs[2].set_yticks(t[::max(1,na//8)])
+    axs[2].set_xlabel("site")
+    p2 = fig.colorbar(mp2,ax=axs[2],orientation="horizontal")
+    axs[2].set_xlim(ix_gm[i0],ix_gm[i1])
+    fig.suptitle("forecast difference : "+pt+" "+op)
+    if dscl:
+        fig.savefig("{}_dkdscl_{}_{}.png".format(model,op,pt))
+    else:
+        fig.savefig("{}_dk_{}_{}.png".format(model,op,pt))
---
+++ plot/plotjh+gh_nest.py
@@ -0,0 +1,132 @@
+import sys
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+plt.rcParams['font.size'] = 16
+from matplotlib.lines import Line2D
+
+op = sys.argv[1]
+model = sys.argv[2]
+na = int(sys.argv[3])
+perts = ["mlef", "mlefw", "var","var_nest",\
+    "mlefcw","mlefy","mlefbe","mlefbm",\
+    "4dvar", "4dmlef"]
+linecolor = {"mlef":'tab:blue',"mlefw":'tab:orange', "var":"tab:olive","var_nest":"tab:brown",\
+        "mlefcw":"tab:green","mlefy":"tab:orange","mlefbe":"tab:red","mlefbm":"tab:pink"}
+marker = {"3d":"o","4d":"x"}
+sigma = {"linear": 1.0, "quadratic": 1.0, "cubic": 1.0, \
+    "quadratic-nodiff": 8.0e-1, "cubic-nodiff": 7.0e-2, \
+    "test":1.0, "abs":1.0, "hint":1.0}
+fig, ax = plt.subplots(nrows=2,ncols=1,figsize=(12,6),constrained_layout=True)
+fig2, ax2 = plt.subplots(nrows=2,ncols=2,figsize=(12,8),constrained_layout=True)
+i = 0
+vmax = 0.0
+lplot = False
+lines = []
+labels = []
+lines2 = []
+labels2 = []
+for pt in perts:
+    cycles = []
+    j_gm = []
+    g_gm = []
+    j_lam = []
+    g_lam = []
+    niter_gm = []
+    niter_lam = []
+    for icycle in range(na):
+        #GM
+        f = "{}_gm_jh_{}_{}_cycle{}.txt".format(model, op, pt, icycle)
+        if not os.path.isfile(f): continue
+        j = np.loadtxt(f)
+        if j.ndim == 2:
+            j_gm.append(j[-1,:])
+        else:
+            j_gm.append(j)
+        f = "{}_gm_gh_{}_{}_cycle{}.txt".format(model, op, pt, icycle)
+        if not os.path.isfile(f): continue
+        g = np.loadtxt(f)
+        if g.ndim == 1:
+            g_gm.append(g[-1])
+            niter_gm.append(g.size)
+        else:
+            g_gm.append(g)
+            niter_gm.append(1)
+        #LAM
+        f = "{}_lam_jh_{}_{}_cycle{}.txt".format(model, op, pt, icycle)
+        if not os.path.isfile(f): continue
+        j = np.loadtxt(f)
+        if j.ndim == 2:
+            j_lam.append(j[-1,:])
+        else:
+            j_lam.append(j)
+        f = "{}_lam_gh_{}_{}_cycle{}.txt".format(model, op, pt, icycle)
+        if not os.path.isfile(f): continue
+        g = np.loadtxt(f)
+        if g.ndim == 1:
+            g_lam.append(g[-1])
+            niter_lam.append(g.size)
+        else:
+            g_lam.append(g)
+            niter_lam.append(1)
+        #
+        cycles.append(icycle)
+    if len(cycles) == 0:
+        print("not exist {}".format(pt))
+        continue
+    lplot=True
+    j_gm = np.array(j_gm)
+    g_gm = np.array(g_gm)
+    j_lam = np.array(j_lam)
+    g_lam = np.array(g_lam)
+    print("{}, GM mean J = {}".format(pt,np.mean(j_gm.sum(axis=1))))
+    print("{}, GM mean dJ = {}".format(pt,np.mean(g_gm)))
+    print("{}, LAM mean J = {}".format(pt,np.mean(j_lam.sum(axis=1))))
+    print("{}, LAM mean dJ = {}".format(pt,np.mean(g_lam)))
+    ax[0].plot(cycles, j_gm[:,0], linestyle="solid", color=linecolor[pt]) #, label=pt+",Jb")
+    lines.append(Line2D([0],[0],color=linecolor[pt]))
+    labels.append(pt+",Jb")
+    ax[0].plot(cycles, j_gm[:,1], linestyle="dashed", color=linecolor[pt]) #, label=pt+",Jo")
+    lines.append(Line2D([0],[0],color=linecolor[pt],linestyle="dashed"))
+    labels.append(pt+",Jo")
+    ax[1].plot(cycles, j_lam[:,0], linestyle="solid", color=linecolor[pt]) #, label=pt+",Jb")
+    #lines.append(Line2D([0],[0],color=linecolor[pt]))
+    #labels.append(pt+",Jb")
+    ax[1].plot(cycles, j_lam[:,1], linestyle="dashed", color=linecolor[pt]) #, label=pt+",Jo")
+    #lines.append(Line2D([0],[0],color=linecolor[pt],linestyle="dashed"))
+    #labels.append(pt+",Jo")
+    if pt=="var_nest":
+        ax[1].plot(cycles, j_lam[:,2], linestyle="dotted", color=linecolor[pt]) #, label=pt+",Jk")
+        lines.append(Line2D([0],[0],color=linecolor[pt],linestyle="dotted"))
+        labels.append(pt+",Jk")
+    ax2[0,0].plot(cycles, g_gm, linestyle="dashdot", color=linecolor[pt]) #,label=pt+r",$\nabla$J")
+    lines2.append(Line2D([0],[0],color=linecolor[pt],linestyle="dashdot"))
+    labels2.append(pt+r",$\nabla$J")
+    ax2[0,1].plot(cycles, niter_gm, color=linecolor[pt])
+    ax2[1,0].plot(cycles, g_lam, linestyle="dashdot", color=linecolor[pt]) #,label=pt+r",$\nabla$J")
+    ax2[1,1].plot(cycles, niter_lam, color=linecolor[pt])
+    #lines.append(Line2D([0],[0],color=linecolor[pt],linestyle="dashdot"))
+    #labels.append(pt+r",$\nabla$J")
+if lplot:
+    ax[0].set(xlabel="analysis cycles", ylabel="J",
+        title=op+" GM")
+    ax2[0,0].set(xlabel="analysis cycles", ylabel=r"$\nabla$J",
+        title=op+" GM")
+    ax2[0,1].set_ylabel('iteration number',color='red')
+    ax[1].set(xlabel="analysis cycles", ylabel="J",
+        title=op+" LAM")
+    ax2[1,0].set(xlabel="analysis cycles", ylabel=r"$\nabla$J",
+        title=op+" LAM")
+    ax2[1,1].set_ylabel('iteration number',color='red')
+    for ax1 in ax.flatten():
+        ax1.set_yscale("log")
+        ax1.set_xlim(-1,100)
+    for ax1 in ax2[:,0]:
+        ax1.set_yscale("log")
+        ax1.set_xlim(-1,100)
+    for ax1 in ax2[:,1]:
+        ax1.set_xlim(-1,100)
+    ax[1].legend(lines,labels,ncol=2)
+    ax2[0,0].legend(lines2,labels2)
+    fig.savefig("{}_jh_{}.png".format(model, op))
+    fig2.savefig("{}_gh_{}.png".format(model, op))
